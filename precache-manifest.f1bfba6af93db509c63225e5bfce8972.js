704a380f1a0f0359d5e07bc2db05922f6a39ae08
Patrick Whitehouse <patrick1996@gmail.com>
2019-02-28 21:07:30 +0000

    Updates

---
+++ precache-manifest.f1bfba6af93db509c63225e5bfce8972.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e34478c7915db734a5c",
+    "url": "/react-fifa-generator/static/js/runtime~main.0e34478c.js"
+  },
+  {
+    "revision": "3a012aff2311463afb3b",
+    "url": "/react-fifa-generator/static/js/main.3a012aff.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/react-fifa-generator/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "3a012aff2311463afb3b",
+    "url": "/react-fifa-generator/static/css/main.a87e9ebc.chunk.css"
+  },
+  {
+    "revision": "5940dc18820115795ceae4a7a15b92f0",
+    "url": "/react-fifa-generator/index.html"
+  }
+];
